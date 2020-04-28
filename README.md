b879415643f6f7980898e542a701d57a8e4c6d4a
jinkyuhan <gkswlsrb95@gmail.com>
2020-04-29 08:33:00 +0900

    deploy: to pypi

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Google Images Download
+# Google Images Download using Selenium
+
+
+## Usage
+
+```python3
+import gids
+
+config = {
+    'driver_path': './chromedriver',
+    'headless': True,
+    'window-size': '720x480',
+    'disable_gpu': False
+}
+
+item = {
+    'keyword': '햇반',
+    'limit': 10,
+    'download_context': './data',
+    'path': './'
+}
+
+items = [item]
+
+downloader = build(config)
+
+downloader.download(items)
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='google-images-download-selenium',
+    version='1.0',
+    license='',
+    author_email="gkswlsrb95@gmail.com",
+    description="Google-images-downloader module using selenium",
+    long_description=open('README.md').read(), 
+    url="https://github.com/jinkyuhan/google-images-download",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
